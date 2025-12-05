219b8f780e8b3f6ccc9af4b2384b706b9575fa5b
John France <yemsz07@gmail.com>
2025-12-05 21:41:45 +0800

    This is my new style of Portfolio

--- src/portfolio/portfolio/urls.py
+++ src/portfolio/portfolio/urls.py
@@ -21,11 +21,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]"""
 
+from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from . import views
 
+
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", views.index, name="index"),
 ]
 
--- src/portfolio/portfolio/views.py
+++ src/portfolio/portfolio/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render 
+from posts.models import Post
 
 def index(request):
+    posts = Post.objects.all()
     context = {
-        'title': 'Portfolio'
+        'title': 'My First Portfolio',
+        'posts': posts
     }
     return render(request, 'index.html', context)
 
